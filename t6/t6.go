9c6602499506ab853dd2dcf120dcf42107295ebb
NikolaySh <magginibuu69@gmail.com>
2022-07-05 00:52:29 +0300

    t6

--- t6/t6.go
+++ t6/t6.go
@@ -1,60 +1,73 @@
 package main
 
+import "fmt"
+
 /*
 Реализовать все возможные способы остановки выполнения горутины.
 */
 
-//Первый способ - ожидание сигнала завершения
+//Первый способ - остановка по сигналу завершения
 func main() {
 	quit := make(chan bool)
-	for i := 0; i >= 0; i++ {
-		go func(i int) {
-
+	go func() {
+		fmt.Println("В горутине")
+		for {
 			select {
 			case <-quit:
 				return
 			default:
 
+				//Что-нибудь делаем
 			}
-		}(i)
-	}
+		}
+	}()
 	//..
+	fmt.Println("Перед выходом")
 	quit <- true
+	fmt.Println("После выхода")
 }
 
-/* //Второй способ отсановку по прошествии времени
+/*
+//Второй способ: остановка по таймеру
 func main() {
 
-	to := time.After(1 * time.Second)
+	timeIsOver := time.After(1 * time.Second)
 	done := make(chan bool)
 
 	go func() {
 		for {
 			select {
-			case <-to:
+			case <-timeIsOver:
 				fmt.Println("Время истекло")
 				done <- true
 				return
 
 			}
 		}
-		}()
+	}()
 
-		<-done
-	}
+	<-done
+}
 */
-
-/* //Третий способ ожидание контекста
+/*
+//Третий способ ожидание таймера контекста
 func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	var count int32 = 1
-	for i := 1; i > 0; i++ {
-		go func(i int, ctx context.Context) {
-			defer cancel()
-			atomic.AddInt32(&count, 1)
-		}(i, ctx)
-	}
+	go func(ctx context.Context) {
+		for {
+			fmt.Println("мы тут")
+			select {
+			case <-ctx.Done():
+				fmt.Println("Время истекло 1")
+				return
+			default:
+				fmt.Println("Мы тут 2")
+			}
+
+		}
+	}(ctx)
+	time.Sleep(5 * time.Second)
+	fmt.Println("Время истекло 2")
 }
 */
